d35334dd3b9cf47e3cc2b7348f32743648f1ec4d
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2021-03-26 13:26:36 -0400

    Updates

---
+++ precache-manifest.a3cc4d9338022ced815008e803b31978.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f2c0c8b150926c982f890807cecc48",
+    "url": "/deploy-portfolio/index.html"
+  },
+  {
+    "revision": "050597e7eea74776f2a7",
+    "url": "/deploy-portfolio/static/css/main.cee1f0b2.chunk.css"
+  },
+  {
+    "revision": "980d6db60f0d5056c1ed",
+    "url": "/deploy-portfolio/static/js/2.e216d209.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deploy-portfolio/static/js/2.e216d209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050597e7eea74776f2a7",
+    "url": "/deploy-portfolio/static/js/main.d36cb7fd.chunk.js"
+  },
+  {
+    "revision": "3708271e407397598d64",
+    "url": "/deploy-portfolio/static/js/runtime-main.ccba90f6.js"
+  },
+  {
+    "revision": "ce98b3c327a880463e160955d07f26df",
+    "url": "/deploy-portfolio/static/media/wendyportfolio.ce98b3c3.jpeg"
+  }
+]);
